61de1022af3188b475881a66bc181a87726fa9e2
WilliamNwoke <willteey@gmail.com>
2021-08-12 23:45:19 +0100

    Day001 instroductions

--- day001/cppEncaps.cpp
+++ day001/cppEncaps.cpp
@@ -1,7 +1,5 @@
 /*
-    Author: William Nwoke
-
-
+    Author: William
 */
 
 #include <iostream>
@@ -11,6 +9,7 @@ using namespace std;
 class Employee{
     private:
         int salary;
+
     public:
         void setSalary(int s){
             salary = s;
@@ -19,13 +18,11 @@ class Employee{
         int getSalary(){
             return salary;
         }
-
 };
 
 int main(){
     Employee myObj;
-    myObj.setSalary(50000);
-    cout << "$"<<myObj.getSalary()<< "\n";
 
-    return 0;
+    myObj.setSalary(50000);
+    cout << "$" << myObj.getSalary() <<"\n";
 }
