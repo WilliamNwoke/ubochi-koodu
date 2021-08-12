cf52b63eab5e22fe987b0483e11f45342785be05
WilliamNwoke <willteey@gmail.com>
2021-08-12 23:36:04 +0100

    day001

---
+++ day001/cppEncaps1.cpp
@@ -0,0 +1,31 @@
+/*
+    Author: William Nwoke
+
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class Employee{
+    private:
+        int salary;
+    public:
+        void setSalary(int s){
+            salary = s;
+        }
+
+        int getSalary(){
+            return salary;
+        }
+
+};
+
+int main(){
+    Employee myObj;
+    myObj.setSalary(50000);
+    cout << "$"<<myObj.getSalary()<< "\n";
+
+    return 0;
+}
